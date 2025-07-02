d99b109f46a0f1a3ad5e17f0bae80ce25ed0bbc6
AstroMT <81778387+AstroMT@users.noreply.github.com>
2025-07-01 21:32:39 -0400

    Add files via upload

--- BMIcalc.py
+++ BMIcalc.py
@@ -7,18 +7,24 @@ from kivy.uix.label import Label
 class MyApp(App):
     def build(self):
         layout = BoxLayout(orientation = "vertical")
-        weight_input = TextInput(readonly = True, halign = "left", font_size = 50, hint_text = "Enter weight in kg")
-        height_input = TextInput(readonly = False, halign = "left", font_size = 50, hint_text = "Enter height in cm")
-        button = Button(text="Calculate BMI")
-        display_label = Label(text="BMI: ", font_size = 24, color=(1,1,1,1), size_hint=(1, 0.2))
+        self.weight_input = TextInput(readonly = False, halign = "left", font_size = 50, hint_text = "Enter weight in kg")
+        self.height_input = TextInput(readonly = False, halign = "left", font_size = 50, hint_text = "Enter height in cm")
+        button = Button(text="Calculate BMI", on_press = self.calculate_bmi)
+        self.display_label = Label(text="BMI: ", font_size = 24, color=(1,1,1,1), size_hint=(1, 0.2))
         
-        layout.add_widget(weight_input)
-        layout.add_widget(height_input)
+        layout.add_widget(self.weight_input)
+        layout.add_widget(self.height_input)
         layout.add_widget(button)
-        layout.add_widget(display_label)
+        layout.add_widget(self.display_label)
 
 
         return layout
+    def calculate_bmi (self, instance):
+        weight = float(self.weight_input.text)
+        height = float(self.height_input.text) / 100
+        bmi = weight / (height * height)
+        self.display_label.text = f"BMI: {bmi:.2f}"
+
 
 
 if __name__ == "__main__":
